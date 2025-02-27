3d950171ce107bf4de17793642c9d032f3258580
vmpierluisi <vmpierluisi@gmail.com>
2025-02-27 22:07:34 +0000

    Added Open AI API key

--- supabase/functions/analyze-portfolio/index.ts
+++ supabase/functions/analyze-portfolio/index.ts
@@ -2,7 +2,7 @@
 import "https://deno.land/x/xhr@0.1.0/mod.ts";
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 
-const openAIApiKey = Deno.env.get('OPENAI_API_KEY');
+const openAIApiKey = Deno.env.get('sk-proj-odE1t2vaQqPefBYFxcOAT3BlbkFJ2XQiR2tPVkwkGEBWlXcz');
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
